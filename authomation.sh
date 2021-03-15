41efe0206b8470dc50a41a231ca06a6f6b17e00b
Mukul <mukulanand.aw@gmail.com>
2021-03-15 22:37:47 +0530

    Task 2 final script

--- authomation.sh
+++ authomation.sh
@@ -8,11 +8,18 @@ sudo apt update -y
             sudo apt-get install apache2
             sudo systemctl enable apache2
     fi
-	
+
+
+    if [ $(/etc/init.d/apache2 status |grep 'active (running)' | wc -l) != 1 ]
+        then
+                echo "Starting Apache2"
+                sudo systemctl start apache2
+    fi
+
+
 timestamp=$(date '+%d%m%Y-%H%M%S')
 echo "$timestamp"
 myname="Mukul"
 s3_bucket="upgradmukul"
-tar -czvf /tmp/$myname-httpd-logs-$timestamp.tar /var/log/apache2/error.log
+tar -czvf /tmp/$myname-httpd-logs-$timestamp.tar /var/log/apache2/*.log
 aws s3 cp /tmp/${myname}-httpd-logs-${timestamp}.tar s3://${s3_bucket}/${myname}-httpd-logs-${timestamp}.tar
-
