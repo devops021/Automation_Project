6a3c7ddd118a5c2aa9a32ab180fa54b1b748448f
Mukul <mukulanand.aw@gmail.com>
2021-03-16 00:10:06 +0530

    Task 3 final script

--- authomation.sh
+++ authomation.sh
@@ -4,7 +4,7 @@ sudo apt update -y
     dpkg -s apache2 &> /dev/null
 
     if [ $? -ne 0 ]
-        then
+   then
             sudo apt-get install apache2
             sudo systemctl enable apache2
     fi
@@ -22,4 +22,16 @@ echo "$timestamp"
 myname="Mukul"
 s3_bucket="upgradmukul"
 tar -czvf /tmp/$myname-httpd-logs-$timestamp.tar /var/log/apache2/*.log
+if [[ ! -f "/var/www/html/inventory.html" ]]
+  then
+    echo "File inventory.html does not exist. "
+    touch /var/www/html/inventory.html
+    sudo printf "<div>Log Type &nbsp Date Created &nbsp Type &nbsp Size </div> " >> /var/www/html/inventory.html
+
+fi
+
+FILENAME=/tmp/${myname}-httpd-logs-${timestamp}.tar
+FILESIZE=$(stat -c%s "$FILENAME")
+
+sudo printf "<div>httpd-logs &nbsp $timestamp &nbsp tar &nbsp $FILESIZE K</div>" >> /var/www/html/inventory.html
 aws s3 cp /tmp/${myname}-httpd-logs-${timestamp}.tar s3://${s3_bucket}/${myname}-httpd-logs-${timestamp}.tar
